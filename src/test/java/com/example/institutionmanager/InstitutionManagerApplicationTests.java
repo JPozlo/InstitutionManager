59e049483be370c27dfe11c19a7bf905a71771b4
Collins Oslo <ozlocollins@gmail.com>
2021-11-20 11:14:29 +0300

    Initial Commit after Setup

---
+++ src/test/java/com/example/institutionmanager/InstitutionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.institutionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
